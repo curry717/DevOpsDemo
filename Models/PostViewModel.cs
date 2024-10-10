7b59c71d86487f88ec67f62ce2e68c29f671449b
Courage Garh <garhcourage717@gmail.com>
2024-10-10 17:33:44 +0100

    first

---
+++ Models/PostViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace DevOpsDemo.Models
+{
+    public partial class PostViewModel
+    {
+        public int PostId { get; set; }
+        public string? Title { get; set; }  // Nullable
+        public string? Description { get; set; }  // Nullable
+        public string? Author { get; set; }  // Nullable
+    }
+}
